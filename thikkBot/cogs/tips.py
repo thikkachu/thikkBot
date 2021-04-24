6a800bc6cfc4cc85b2160d2aaa0cbab81f8c9b60
thikkachu <spaghettilop@gmail.com>
2021-04-24 00:32:08 -1000

    hehe funny tip

--- thikkBot/cogs/tips.py
+++ thikkBot/cogs/tips.py
@@ -9,7 +9,7 @@ class Tips(commands.Cog):
     def __init__(self, bot, config):
         self.bot = bot
         self.config = config[__name__.split(".")[-1]]
-        self.tips = ["Only admins and the song requester can immediately skip songs. Everybody else will have to vote!",
+        self.tips = ["You are most likely homosexual!",
                      f"You can check out my source code here: {self.config['github_url']}"]
 
     @commands.command()
